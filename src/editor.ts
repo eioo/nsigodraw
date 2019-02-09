dc835387de5409e9985cd3693f173cd81822585e
Kallu609 <khirvola@gmail.com>
2019-02-09 23:06:57 +0200

    add more keybinds and tidy up code

--- src/editor.ts
+++ src/editor.ts
@@ -17,6 +17,7 @@ const brush = {
   fgColor: 'white',
   bgColor: 'black',
 };
+
 const cursor = {
   x: 15,
   y: 10,
@@ -24,9 +25,7 @@ const cursor = {
 
 export function init() {
   createCanvas();
-
-  window.onkeydown = e => handleKeyPress(e);
-
+  window.onkeydown = handleKeyDown;
   requestAnimationFrame(mainLoop);
 }
 
@@ -35,7 +34,6 @@ function createCanvas() {
   canvas.width = tilesH * tileW;
   canvas.height = tilesV * tileH;
   ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
   document.body.appendChild(canvas);
 }
 
@@ -44,7 +42,7 @@ function mainLoop() {
   draw();
 }
 
-function handleKeyPress(e: KeyboardEvent) {
+function handleKeyDown(e: KeyboardEvent) {
   if (!keycode.isEventKey(e, 'F5') && !keycode.isEventKey(e, 'F12')) {
     e.preventDefault();
   }
@@ -58,7 +56,7 @@ function handleKeyPress(e: KeyboardEvent) {
     cursor.x -= 1;
   }
 
-  if (keycode.isEventKey(e, 'Right') && cursor.x !== tilesH) {
+  if (keycode.isEventKey(e, 'Right') && cursor.x !== tilesH - 1) {
     controlKey = true;
     cursor.x += 1;
   }
@@ -68,9 +66,24 @@ function handleKeyPress(e: KeyboardEvent) {
     cursor.y -= 1;
   }
 
-  if (keycode.isEventKey(e, 'Down') && cursor.y !== tilesV) {
+  if (keycode.isEventKey(e, 'Down') && cursor.y !== tilesV - 1) {
     controlKey = true;
     cursor.y += 1;
+  }
+
+  if (keycode.isEventKey(e, 'Space') && cursor.x !== tilesH - 1) {
+    controlKey = true;
+    addCharacter(' ');
+    cursor.x += 1;
+  }
+
+  if (keycode.isEventKey(e, 'Enter')) {
+    controlKey = true;
+    cursor.x = 0;
+
+    if (cursor.y !== tilesV - 1) {
+      cursor.y += 1;
+    }
   }
 
   // Edit keys
@@ -86,28 +99,29 @@ function handleKeyPress(e: KeyboardEvent) {
   }
 
   // Character keys
+  // Todo: Hard-coded. Add charsets you can select
   if (keycode.isEventKey(e, 'F1')) {
     advanceCursor = true;
     controlKey = true;
-    addCharacter('\u{00db}');
+    addCharacter('\u{00b0}');
   }
 
   if (keycode.isEventKey(e, 'F2')) {
     advanceCursor = true;
     controlKey = true;
-    addCharacter('\u{00b2}');
+    addCharacter('\u{00b1}');
   }
 
   if (keycode.isEventKey(e, 'F3')) {
     advanceCursor = true;
     controlKey = true;
-    addCharacter('\u{00b1}');
+    addCharacter('\u{00b2}');
   }
 
   if (keycode.isEventKey(e, 'F4')) {
     advanceCursor = true;
     controlKey = true;
-    addCharacter('\u{00b0}');
+    addCharacter('\u{00db}');
   }
 
   const validChar =
@@ -131,6 +145,10 @@ function handleKeyPress(e: KeyboardEvent) {
 
 function getCharIndexAtPos(x: number, y: number) {
   return charArray.findIndex(c => c.x === cursor.x && c.y === cursor.y);
+}
+
+function getCharAtCursor(): ICharacter | undefined {
+  return charArray.find(c => c.x === cursor.x && c.y === cursor.y);
 }
 
 function addCharacter(charRepr: string) {
@@ -174,6 +192,7 @@ function draw() {
   ctx.fillText('\u{00db}\u{00b2}\u{00b1}\u{00b0}', 0, blockOffsetY);
 
   // Cursor
-  ctx.fillStyle = 'white';
+  // Todo: inverse cursor color (ctx.globalCompositeOperation('lighten'))
+  ctx.fillStyle = brush.fgColor;
   ctx.fillRect(cursor.x * tileW, cursor.y * tileH + tileH - 2, tileW, 2);
 }
